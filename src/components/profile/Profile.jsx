391f0a21f1191c1040692ab6d869b117128a897a
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2022-06-13 15:07:11 -0500

    Profile Styles Header

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { AccountCircle } from "@mui/icons-material";
 import "./styles/Profile.css";
+import { Col, Row } from "react-bootstrap";
 
 const Profile = () => {
   return (
     <>
       <div className="right-heading">
-        <AccountCircle className=".img-account-circule" />
-        <p className="p2">Profile</p>
+        <Row style={{ padding: "0px 8px 0px 8px" }}>
+          <Col lg="2" style={{ width: "70px" }}>
+            <AccountCircle className="account-circle" />
+          </Col>
+          <Col>
+            <p className="p2">Profile</p>
+          </Col>
+        </Row>
       </div>
     </>
   );
